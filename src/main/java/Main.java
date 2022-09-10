eb9e023e0143e2fea200c1d5f5bb5f4c96dc11fc
f4kanzy <f4kanzyttv@gmail.com>
2022-09-10 09:27:54 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Person> people = getPeople();
+
+        // Imperative approach ❌
+
+//        List<Person> females = new ArrayList<>();
+//        for (Person person : people){
+//            if (person.getGender().equals(Gender.FEMALE)){
+//                females.add(person);
+//            }
+//        }
+//        females.forEach(System.out::println);
+
+
+        // Declarative approach ✅
+
+        // Filter
+        List<Person> females = people.stream()
+                .filter(person -> person.getGender().equals(Gender.FEMALE))
+                .collect(Collectors.toList());
+    //females.forEach(System.out::println);
+
+        // Sort YASA GORE SIRALAMA , AGENIN SONUNA RESERVED EKLERSEK TERS SIRALIYOR
+        //thencomparing get gender ile cinsiyete gore silarama
+        List<Person> sorted = people.stream()
+                .sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getGender).reversed())
+                .collect(Collectors.toList());
+    //sorted.forEach(System.out::println);
+        // All match
+        boolean allMatch = people.stream()
+                .allMatch(person -> person.getAge() > 8);
+    //System.out.println(allMatch);
+        // Any match
+        boolean anyMatch = people.stream()
+                     .anyMatch(person -> person.getAge() > 121);
+    //System.out.println(anyMatch);
+        // None match  listede antonio ismi yoksa true doner
+        boolean noneMatch =people.stream()
+                .noneMatch(person -> person.getName().equals("Antonio"));
+    //System.out.println(noneMatch);
+        // Max listede yasi en buyuk kullaniciyi getirir
+        people.stream()
+                .max(Comparator.comparing(Person::getAge));
+            //.ifPresent(System.out::println);
+        // Min listede yasi en ufak olan kullanici getirir
+        people.stream()
+                .min(Comparator.comparing(Person::getAge));
+               // .ifPresent(System.out::println);
+        // Group Cinsiyete gore gruplama
+        Map<Gender, List<Person>> groupByGender= people.stream()
+                .collect(Collectors.groupingBy(Person::getGender));
+
+//        groupByGender.forEach((gender, people1) -> {
+//            System.out.println(gender);
+//            people1.forEach(System.out::println);
+//        });
+        // Listedeki yasi en buyuk kadin kullanici getiriyor
+        Optional<String>oldestFemaleAge=people.stream()
+                .filter(person -> person.getGender().equals(Gender.FEMALE))
+                .max(Comparator.comparing(Person::getAge))
+                .map(Person::getName);
+        oldestFemaleAge.ifPresent(System.out::println);
+    }
+
+    private static List<Person> getPeople() {
+        return List.of(
+                new Person("Antonio", 20, Gender.MALE),
+                new Person("Alina Smith", 33, Gender.FEMALE),
+                new Person("Helen White", 57, Gender.FEMALE),
+                new Person("Alex Boz", 14, Gender.MALE),
+                new Person("Jamie Goa", 99, Gender.MALE),
+                new Person("Anna Cook", 7, Gender.FEMALE),
+                new Person("Zelda Brown", 120, Gender.FEMALE)
+        );
+    }
+
+}
